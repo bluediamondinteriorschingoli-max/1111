a7738311ada7681fbca61c362cd51f3a7058f425
bluediamondinteriorschingoli-max <bluediamondinteriorschingoli@gmail.com>
2025-11-01 15:36:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_krishnalayam_model_for_carpentry1_2025-11-01-15-29-30",
+      "name": "Panorama_Krishnalayam_Model_For_Carpentry[1]_2025-11-01-15-29-30",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8028281355018132,
+        "pitch": -0.03835056514038726,
+        "fov": 1.3115971738307333
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0450246409533843,
+          "pitch": -0.05444668995357915,
+          "rotation": 0.7853981633974483,
+          "target": "1-panorama_krishnalayam_model_for_carpentry1_2025-11-01-15-25-35"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama_krishnalayam_model_for_carpentry1_2025-11-01-15-25-35",
+      "name": "Panorama_Krishnalayam_Model_For_Carpentry[1]_2025-11-01-15-25-35",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.4092744527272547,
+        "pitch": 0.10216091863277477,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.713585522769078,
+          "pitch": 0.11968536739963298,
+          "rotation": 4.71238898038469,
+          "target": "0-panorama_krishnalayam_model_for_carpentry1_2025-11-01-15-29-30"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "111111",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
